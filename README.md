8f46a225b559a5e0a6a8bc7fc29efdb521bbf630
pyCynical <enxfusion@gmail.com>
2018-10-20 02:29:53 +0100

    Changes

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # smspva
 Python wrapper for http://smspva.com
+Docs http://smspva.com/new_theme_api.html
 
 You can install this package using git or pip for example -
 
@@ -18,4 +19,47 @@ OR
 from smspva import *
 sms_pva = smsPvaAPI(API_KEY)
 print(sms_pva.get_balance("opt4"))
+```
+Available Methods: 
+
+```
+get_balance(service)
+:param service: Service name from the service list
+def get_userinfo(self, service: str = None, _id: int = None, operator: str = None)
+get_userinfo(service,_id, operator)
+:param service: Optional parameter to include Service name from the service list
+:param _id: Optional parameter to include request ID
+:param operator: (Additional parameter can be omitted) Beeline_RU, MTS_RU, Megafon_RU, Beeline_KZ, Tele2_KZ, Activ_KZ, Altel_KZ, Lifecell_UA, Kyivstar_UA, Vodafone_UA
+
+get_count(service,country)
+:param service: Service name from the service list 
+:param country: Optional but you can include the country code
+
+get_number(country, service)
+:param country: Country code from country list
+:param service: Service name from the service list
+
+get_ban(service, _id)
+:param service: Service name from the service list
+:param _id: Request ID 
+
+get_sms(country, service, _id, sms)
+:param country: Country code from country list 
+:param service: Service from service list 
+:param _id: Request ID
+:param sms: Optional - If you want to get re-SMS without closing the order (Code Refinement), then just on the method get_sms add additional parameter sms=sms
+
+get_denial(country,service,_id)
+:param country: Country code from country list 
+:param service: Service from service list 
+:param _id: Request ID 
+
+get_proverka(service, number)
+:param service: Service from service list 
+:param number: Number 
+
+get_sim(service _id)
+:param service: Service from service list 
+:param _id: Request ID 
+
 ```
--- smspva/__init__.py
+++ smspva/__init__.py
@@ -14,52 +14,52 @@ class smsPvaAPI():
 		self.API_ENDPOINT = "http://smspva.com/priemnik.php"
 		self.API_KEY = API_KEY
 	
-	def check_response(self, resp):
+	def check_response(self, resp) -> dict:
 		if resp.status_code != 200 or not is_json(resp.text):
 			return f"There was some problem making the request more info:\nStatus Code: {resp.status_code}\nText: {resp.text}"
 		return resp.json()
 
-	def get_balance(self, service):
+	def get_balance(self, service: str = None) -> dict:
 		params = dict(metod="get_balance",service=service,apikey=self.API_KEY)
 		r = requests.get(self.API_ENDPOINT,params=params)
 		return self.check_response(r)
 
-	def get_userinfo(self, service, _id=None,operator=None):
+	def get_userinfo(self, service: str = None, _id: int = None, operator: str = None) -> dict:
 		params = dict(metod="get_userinfo",service=service,apikey=self.API_KEY) 
 		r = requests.get(self.API_ENDPOINT,params=params)
 		return self.check_response(r)
 
-	def get_count(self, service, country=None):
+	def get_count(self, service: str , country: str = None) -> dict:
 		params = dict(metod="get_count_new",service=service,apikey=self.API_KEY,country=country)
 		r = requests.get(self.API_ENDPOINT,params=params)
 		return self.check_response(r)
 
-	def get_number(self, country, service):
+	def get_number(self, country: str, service: str = None) -> dict:
 		params = dict(metod="get_number",country=country,service=service,apikey=self.API_KEY)
 		r = requests.get(self.API_ENDPOINT,params=params)
 		return self.check_response(r)
 
-	def get_ban(self, service, _id):
+	def get_ban(self, service: str, _id: int) -> dict:
 		params = dict(metod="ban", service=service, apikey=self.API_KEY, id=_id)
 		r = requests.get(self.API_ENDPOINT,params=params)
 		return self.check_response(r)
 
-	def get_sms(self, country, service, _id,sms=None):
+	def get_sms(self, country: str, service: str, _id: int , sms: str = None) -> dict:
 		params = dict(metod="get_sms", country=country, service=service, id=_id, apikey=self.API_KEY)
 		r = requests.get(self.API_ENDPOINT,params=params)
 		return self.check_response(r)
 
-	def get_denial(self, country, service, _id):
+	def get_denial(self, country: str, service: str, _id: int) -> dict:
 		params = dict(metod="denial", country=country, service=service, id=_id, apikey=self.API_KEY)
 		r = requests.get(self.API_ENDPOINT,params=params)
 		return self.check_response(r)
 
-	def get_proverka(self, service, number):
+	def get_proverka(self, service: str, number: str) -> dict:
 		params = dict(metod="get_proverka", service=service, number=number, apikey=self.API_KEY)
 		r = requests.get(self.API_ENDPOINT,params=params)
 		return self.check_response(r)
 
-	def get_sim(self, service, _id):
+	def get_sim(self, service: str, _id: int) -> dict:
 		params = dict(metod="balance_sim", service=service, id=_id, apikey=self.API_KEY)
 		r = requests.get(self.API_ENDPOINT,params=params)
 		return self.check_response(r)
