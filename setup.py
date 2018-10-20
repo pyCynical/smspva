96fd6b3e7146f845e8f762ce92a4ed0e0ecf8ecd
pyCynical <enxfusion@gmail.com>
2018-10-20 01:23:10 +0100

    Added check_resp to avoid repeating code

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 
 setup(
-    name='smspva API',
+    name='smspva',
     version='0.0.1',
     description='An API wrapper for the website smspva',
-    license='GNU General Public License v3.0',
-    packages=['requests','json'],
+    license='GNU',
+    install_requires=["requests"],
     author='pyCynical',
     author_email='enxfusion@gmail.com',
     keywords=['smspva','sms'],
-    url='https://github.com/pyCynical/smspva'
+    url='https://github.com/pyCynical/smspva',
+    packages = find_packages(exclude=["docs","tests"])
+
 )
-
-
--- smspva/__init__.py
+++ smspva/__init__.py
@@ -1,62 +1,68 @@
-import requests,json
+import requests, json
 
 
-def is_json(self,obj):
+def is_json(obj):
 	try:
 		json_object = json.loads(obj)
-	except ValueError as e:
+	except ValueError:
 		return False
 	return True
 
 
 class smsPvaAPI():
-	def __init__(self,API_KEY: str):
-		self.base_url = "http://smspva.com/priemnik.php"
+	def __init__(self,API_KEY):
+		self.API_ENDPOINT = "http://smspva.com/priemnik.php"
 		self.API_KEY = API_KEY
-		
+	
+	def check_response(self, resp):
+		if resp.status_code != 200 or not is_json(resp.json()):
+			return f"There was some problem making the request more info:\nStatus Code: {resp.status_code}\nText: {resp.text}"
+		return resp.json()
+
 	def get_balance(self, service):
 		params = dict(metod="get_balance",service=service,apikey=self.API_KEY)
-		r = requests.get(self.base_url,params=params)
-		return r.json() if is_json(r.text) else r.text
+		r = requests.get(self.API_ENDPOINT,params=params)
+		return check_response(r)
 
 	def get_userinfo(self, service, _id=None,operator=None):
 		params = dict(metod="get_userinfo",service=service,apikey=self.API_KEY) 
-		r = requests.get(self.base_url,params=params)
-		return r.json() if is_json(r.text) else r.text
+		r = requests.get(self.API_ENDPOINT,params=params)
+		return check_response(r)
 
 	def get_count(self, service, country=None):
 		params = dict(metod="get_count_new",service=service,apikey=self.API_KEY,country=country)
-		r = requests.get(self.base_url,params=params)
-		return r.json() if is_json(r.text) else r.text
+		r = requests.get(self.API_ENDPOINT,params=params)
+		return check_response(r)
 
 	def get_number(self, country, service):
 		params = dict(metod="get_number",country=country,service=service,apikey=self.API_KEY)
-		r = requests.get(self.base_url,params=params)
-		return r.json() if is_json(r.text) else r.text
+		r = requests.get(self.API_ENDPOINT,params=params)
+		return check_response(r)
 
 	def get_ban(self, service, _id):
 		params = dict(metod="ban", service=service, apikey=self.API_KEY, id=_id)
-		r = requests.get(self.base_url,params=params)
-		return r.json() if is_json(r.text) else r.text
+		r = requests.get(self.API_ENDPOINT,params=params)
+		return check_response(r)
 
 	def get_sms(self, country, service, _id,sms=None):
 		params = dict(metod="get_sms", country=country, service=service, id=_id, apikey=self.API_KEY)
-		r = requests.get(self.base_url,params=params)
-		return r.json() if is_json(r.text) else r.text
+		r = requests.get(self.API_ENDPOINT,params=params)
+		return check_response(r)
 
 	def get_denial(self, country, service, _id):
 		params = dict(metod="denial", country=country, service=service, id=_id, apikey=self.API_KEY)
-		r = requests.get(self.base_url,params=params)
-		return r.json() if is_json(r.text) else r.text
+		r = requests.get(self.API_ENDPOINT,params=params)
+		return check_response(r)
 
 	def get_proverka(self, service, number):
 		params = dict(metod="get_proverka", service=service, number=number, apikey=self.API_KEY)
-		r = requests.get(self.base_url,params=params)
-		return r.json() if is_json(r.text) else r.text
+		r = requests.get(self.API_ENDPOINT,params=params)
+		return check_response(r)
 
 	def get_sim(self, service, _id):
 		params = dict(metod="balance_sim", service=service, id=_id, apikey=self.API_KEY)
-		r = requests.get(self.base_url,params=params)
-		return r.json() if is_json(r.text) else r.text
+		r = requests.get(self.API_ENDPOINT,params=params)
+		return check_response(r)
 
 
+"service","id","number","country","sms","operator"
